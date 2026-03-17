b1ce21cc7bf41f750e66d1acf88f07b8bf4d07e8
richardpizem-ctrl <richardpizem@gmail.com>
2026-03-17 08:26:54 +0100

    Create device_manager.py

---
+++ src/device_manager.py
@@ -0,0 +1,13 @@
+# Device manager
+
+class DeviceManager:
+    def __init__(self):
+        self.devices = []
+
+    def scan_devices(self):
+        # TODO: Implement MIDI device scanning
+        pass
+
+    def select_device(self, device_name):
+        # TODO: Implement device selection
+        pass
