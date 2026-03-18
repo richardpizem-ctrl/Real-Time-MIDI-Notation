25e256470ccf4ba90d5de7393cd997517102905e
richardpizem-ctrl <richardpizem@gmail.com>
2026-03-18 05:24:55 +0100

    Create notation_processor.py

---
+++ notation_engine/notation_processor.py
@@ -0,0 +1,12 @@
+class NotationProcessor:
+    def __init__(self):
+        # Inicializácia procesora notácie
+        pass
+
+    def process_midi_event(self, event):
+        # Tu budeme neskôr spracovávať MIDI udalosti
+        pass
+
+    def quantize_notes(self):
+        # Tu bude logika kvantovania
+        pass
