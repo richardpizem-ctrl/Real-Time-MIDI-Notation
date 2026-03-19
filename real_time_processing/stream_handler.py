52907f802f7626e15524595fd672130e3514ec43
richardpizem-ctrl <richardpizem@gmail.com>
2026-03-19 17:52:56 +0100

    Update stream_handler.py

--- real_time_processing/stream_handler.py
+++ real_time_processing/stream_handler.py
@@ -1,44 +1,46 @@
-import time
-from notation_engine.midi_note_mapper import MidiNoteMapper
+# real_time_processing/stream_handler.py
+
+from notation_engine.chord_detector import detect_chord
+
 
 class StreamHandler:
     def __init__(self):
-        # MIDI → Note mapper
-        self.mapper = MidiNoteMapper()
+        # Zoznam aktuálne stlačených MIDI nôt
+        self.active_notes = []
 
-        # Callback keď mapper vytvorí hotovú notu
-        self.mapper.on_note_created = self._on_note_created
+    def process_midi_message(self, message):
+        """
+        Spracuje prichádzajúcu MIDI správu.
+        message.type môže byť "note_on" alebo "note_off"
+        message.note je číslo MIDI noty (0–127)
+        """
+
+        # NOTE ON
+        if message.type == "note_on":
+            if message.note not in self.active_notes:
+                self.active_notes.append(message.note)
+
+        # NOTE OFF
+        elif message.type == "note_off":
+            if message.note in self.active_notes:
+                self.active_notes.remove(message.note)
+
+        # --- Detekcia akordu po každej zmene ---
+        chord = detect_chord(self.active_notes)
+        if chord:
+            print(f"Detected chord: {chord.name}")
+        else:
+            print("No chord detected")
 
     def _on_note_created(self, note):
-        print("NOTE CREATED:", note)
-        # sem neskôr pôjde: notation_engine.add_note(note)
-
-    def process_midi_message(self, msg):
         """
-        msg: objekt z mido alebo rtmidi
-        msg.type: 'note_on' alebo 'note_off'
-        msg.note: pitch
-        msg.velocity: 0–127
-        msg.channel: 0–15
+        Toto je hook, ktorý môžeš neskôr použiť na:
+        - vykreslenie noty
+        - farbenie podľa akordu
+        - posielanie do UI
         """
+        pass
 
-        timestamp = time.time()
-
-        # NOTE ON (velocity > 0)
-        if msg.type == "note_on" and msg.velocity > 0:
-            self.mapper.handle_note_on(
-                pitch=msg.note,
-                velocity=msg.velocity,
-                channel=msg.channel,
-                timestamp=timestamp
-            )
-
-        # NOTE OFF (alebo note_on s velocity 0)
-        elif msg.type in ("note_off", "note_on") and msg.velocity == 0:
-            self.mapper.handle_note_off(
-                pitch=msg.note,
-                channel=msg.channel,
-                timestamp=timestamp
-            )
+              
 
 
