ca47b6160c8ae564076f6f8869a2f46a21ba3370
richardpizem-ctrl <richardpizem@gmail.com>
2026-03-29 06:50:32 +0200

    Update piano_roll_ui.py

--- ui/piano_roll_ui.py
+++ ui/piano_roll_ui.py
@@ -6,61 +6,46 @@ class PianoRollUI:
     BLACK_KEY_WIDTH = 12
     BLACK_KEY_HEIGHT = 80
 
-    FIRST_MIDI_NOTE = 36   # C2
-    LAST_MIDI_NOTE = 96    # C7
+    FIRST_MIDI_NOTE = 36
+    LAST_MIDI_NOTE = 96
 
     def __init__(self, width=1400, height=200):
         self.width = width
         self.height = height
-
-        # Stav klávesov: midi_note → farba
         self.active_keys = {}
-
-        # Prepočítané pozície klávesov
         self.white_keys = []
         self.black_keys = []
         self._calculate_key_positions()
 
-    # ---------------------------------------------------------
-    # VÝPOČET POZÍCIÍ KLÁVES
-    # ---------------------------------------------------------
     def _calculate_key_positions(self):
-        white_key_order = [0, 2, 4, 5, 7, 9, 11]  # C D E F G A B
+        white_key_order = [0, 2, 4, 5, 7, 9, 11]
         black_key_offsets = {
-            1: 0.65,  # C#
-            3: 1.65,  # D#
-            6: 3.65,  # F#
-            8: 4.65,  # G#
-            10: 5.65  # A#
+            1: 0.65,
+            3: 1.65,
+            6: 3.65,
+            8: 4.65,
+            10: 5.65
         }
 
         white_index = 0
 
-        # Biele klávesy
         for midi_note in range(self.FIRST_MIDI_NOTE, self.LAST_MIDI_NOTE + 1):
             note_in_octave = midi_note % 12
-
             if note_in_octave in white_key_order:
                 x = white_index * self.WHITE_KEY_WIDTH
                 rect = pygame.Rect(x, 0, self.WHITE_KEY_WIDTH, self.WHITE_KEY_HEIGHT)
                 self.white_keys.append((midi_note, rect))
                 white_index += 1
 
-        # Čierne klávesy
         for midi_note in range(self.FIRST_MIDI_NOTE, self.LAST_MIDI_NOTE + 1):
             note_in_octave = midi_note % 12
-
             if note_in_octave in black_key_offsets:
                 octave = (midi_note - self.FIRST_MIDI_NOTE) // 12
                 base_white_index = octave * 7
-
                 x = int((base_white_index + black_key_offsets[note_in_octave]) * self.WHITE_KEY_WIDTH)
                 rect = pygame.Rect(x, 0, self.BLACK_KEY_WIDTH, self.BLACK_KEY_HEIGHT)
                 self.black_keys.append((midi_note, rect))
 
-    # ---------------------------------------------------------
-    # HIGHLIGHT / UNHIGHLIGHT
-    # ---------------------------------------------------------
     def highlight_key(self, midi_note, color=(255, 0, 0)):
         self.active_keys[midi_note] = color
 
@@ -68,19 +53,14 @@ class PianoRollUI:
         if midi_note in self.active_keys:
             del self.active_keys[midi_note]
 
-    # ---------------------------------------------------------
-    # KRESLENIE NA SURFACE
-    # ---------------------------------------------------------
     def draw(self, surface):
         surface.fill((30, 30, 30))
 
-        # Biele klávesy
         for midi_note, rect in self.white_keys:
             color = self.active_keys.get(midi_note, (255, 255, 255))
             pygame.draw.rect(surface, color, rect)
             pygame.draw.rect(surface, (0, 0, 0), rect, 2)
 
-        # Čierne klávesy
         for midi_note, rect in self.black_keys:
             color = self.active_keys.get(midi_note, (0, 0, 0))
             pygame.draw.rect(surface, color, rect)
