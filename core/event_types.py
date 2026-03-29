fc9d0259485dcbafa2f6fd026c70c224e0fedf4d
richardpizem-ctrl <richardpizem@gmail.com>
2026-03-29 16:46:39 +0200

    Update event_types.py

--- core/event_types.py
+++ core/event_types.py
@@ -1,27 +1,42 @@
 # Centrálne definované typy udalostí pre celý projekt
+# Stabilizované:
+# - jednotná štruktúra
+# - žiadne duplicity
+# - bezpečné, konzistentné názvy
+# - pripravené pre EventBus a celý systém
 
+# ---------------------------------------------------------
 # MIDI EVENTS
+# ---------------------------------------------------------
 MIDI_NOTE_ON = "midi.note_on"
 MIDI_NOTE_OFF = "midi.note_off"
 MIDI_CONTROL_CHANGE = "midi.cc"
 
+# ---------------------------------------------------------
 # TRACK EVENTS
+# ---------------------------------------------------------
 TRACK_SELECTED = "track.selected"
 TRACK_NAME_CHANGED = "track.name_changed"
 TRACK_COLOR_CHANGED = "track.color_changed"
 TRACK_MUTED = "track.muted"
 TRACK_SOLOED = "track.soloed"
 
+# ---------------------------------------------------------
 # RECORDING EVENTS
+# ---------------------------------------------------------
 NOTE_RECORDED = "recording.note_recorded"
 RECORDING_STARTED = "recording.started"
 RECORDING_STOPPED = "recording.stopped"
 
+# ---------------------------------------------------------
 # EXPORT EVENTS
+# ---------------------------------------------------------
 MIDI_EXPORT_REQUEST = "export.midi_request"
 MIDI_EXPORTED = "export.midi_done"
 
+# ---------------------------------------------------------
 # SYSTEM EVENTS
+# ---------------------------------------------------------
 APP_STARTED = "system.app_started"
 APP_STOPPED = "system.app_stopped"
 ERROR_OCCURRED = "system.error"
