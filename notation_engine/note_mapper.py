9b6d5fe02bb0eb7176b251ca123b0d29020ce871
richardpizem-ctrl <richardpizem@gmail.com>
2026-04-05 13:43:52 +0200

    Update note_mapper.py

--- notation_engine/note_mapper.py
+++ notation_engine/note_mapper.py
@@ -1,6 +1,8 @@
 class NoteMapper:
-    NOTE_NAMES = ["C", "C#", "D", "D#", "E", "F",
-                  "F#", "G", "G#", "A", "A#", "B"]
+    NOTE_NAMES = [
+        "C", "C#", "D", "D#", "E", "F",
+        "F#", "G", "G#", "A", "A#", "B"
+    ]
 
     def __init__(self):
         pass
@@ -9,28 +11,38 @@ class NoteMapper:
         """
         Prevod MIDI čísla (0–127) na názov noty.
         Napr. 60 -> C4, 61 -> C#4, 62 -> D4
+
         Stabilizované:
         - ochrana pred None
         - ochrana pred nevalidnými typmi
         - ochrana pred rozsahom mimo 0–127
+        - bezpečný výpočet oktávy
         """
+        # None → nič nevraciame
         if midi_number is None:
             return None
 
+        # bezpečný cast
         try:
             midi_number = int(midi_number)
         except Exception:
             return None
 
+        # rozsah MIDI
         if midi_number < 0 or midi_number > 127:
             return None
 
-        note_index = midi_number % 12
-        octave = (midi_number // 12) - 1
-
+        # index v rámci oktávy
         try:
+            note_index = midi_number % 12
             note_name = self.NOTE_NAMES[note_index]
         except Exception:
             return None
+
+        # výpočet oktávy (MIDI štandard: 60 = C4)
+        try:
+            octave = (midi_number // 12) - 1
+        except Exception:
+            octave = 0
 
         return f"{note_name}{octave}"
