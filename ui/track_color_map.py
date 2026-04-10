de0ba2a18e424ae8b5e194ebbf6a010ed530aecd
richardpizem-ctrl <richardpizem@gmail.com>
2026-04-10 04:05:45 +0200

    Create track_color_map.py

---
+++ ui/track_color_map.py
@@ -0,0 +1,30 @@
+class TrackColorMap:
+    """
+    Mapovanie farieb pre 16 MIDI stôp podľa Yamaha štandardu.
+    UI aj Renderer používajú tieto farby na konzistentné zobrazenie.
+    """
+
+    def __init__(self):
+        # Farby sú v hex formáte (#RRGGBB)
+        self.colors = [
+            "#FF4B4B",  # Track 1 - Red
+            "#FF8A33",  # Track 2 - Orange
+            "#FFC233",  # Track 3 - Yellow
+            "#F2FF33",  # Track 4 - Lime
+            "#A6FF33",  # Track 5 - Light Green
+            "#33FF57",  # Track 6 - Green
+            "#33FFBD",  # Track 7 - Aqua
+            "#33E3FF",  # Track 8 - Light Blue
+            "#3396FF",  # Track 9 - Blue
+            "#335BFF",  # Track 10 - Deep Blue
+            "#6A33FF",  # Track 11 - Violet
+            "#A833FF",  # Track 12 - Purple
+            "#E633FF",  # Track 13 - Magenta
+            "#FF33C4",  # Track 14 - Pink
+            "#FF337A",  # Track 15 - Rose
+            "#FF334B",  # Track 16 - Red-Pink
+        ]
+
+    def get_color(self, track: int) -> str:
+        """Vráti farbu pre danú stopu (0–15)."""
+        return self.colors[track]
