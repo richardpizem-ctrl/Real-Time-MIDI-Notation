382c724c13c93a47a10a5bc76664a58b77cc8776
richardpizem-ctrl <richardpizem@gmail.com>
2026-05-10 17:51:05 +0200

    Add docstring to tests package initializer
    Add module docstring to indicate test package purpose

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,13 @@
+"""
+Test Package Initializer (v4.0.0)
+
+Tento modul označuje priečinok `tests` ako platný Python balík.
+Slúži pre:
+- jednotkové testy
+- integračné testy
+- testovanie real‑time pipeline
+- izolované testovanie modulov v architektúre v4
+"""
+
+__all__ = []
 
